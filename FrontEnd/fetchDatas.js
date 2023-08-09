8c01a7ab90adba86a0dd9b244c8533ed0da125e3
dg-Tech1 <130319167+dg-Tech1@users.noreply.github.com>
2023-08-09 09:56:47 +0200

    Update fetchDatas.js

--- FrontEnd/fetchDatas.js
+++ FrontEnd/fetchDatas.js
@@ -1,9 +1,9 @@
 // Récupération des données "WORKS" sur l'API.
-const responseWorks = await fetch("http://localhost:5678/api/works");
-const works = await responseWorks.json();
-export {works};
+const responseWorks = await fetch("http://localhost:5678/api/works"); // Attend que la réponse de la requête soit traitée et transformée en JSON.
+const works = await responseWorks.json(); // Exporte les données des travaux ("WORKS") pour être utilisées dans d'autres fichiers.
+export {works};  //exportez les variables works et categories depuis ce module (fichier) pour les rendre dispo et accessible
 
 // Récupération des données "CATEGORIES" sur l'API.
-const responseCategories = await fetch("http://localhost:5678/api/categories");
-const categories = await responseCategories.json();
-export {categories};
+const responseCategories = await fetch("http://localhost:5678/api/categories"); // Attend que la réponse de la requête soit traitée et transformée en JSON.
+const categories = await responseCategories.json();  // Exporte les données des catégories ("CATEGORIES") pour être utilisées dans d'autres fichiers.
+export {categories};  //exportez les variables works et categories depuis ce module (fichier) pour les rendre dispo et accessible
